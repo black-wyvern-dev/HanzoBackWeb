e635f8303a1f7440ca679df83c7951844c395ed2
Vraj <vdesai18@stevens.edu>
2021-06-15 18:27:28 +0900

    shill migration create

---
+++ database/migrations/2014_10_12_000000_create_shills_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shills', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+
+            $table->increments('id')->unsigned();
+            $table->string('name');
+            $table->string('hadle')->unique();
+            $table->string('content');
+            $table->string('image');
+            $table->string('avatar');
+            $table->integer('comment');
+            $table->integer('retweet');
+            $table->integer('heart');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('musics');
+    }
+}
